7b2925a6d8e160db68ad47da560dd212a5852395
Pepe Una <xmak@studioartlan.com>
2023-07-03 20:49:58 -0500

    add translations and AieError

---
+++ src/aiecommon/Errors.py
@@ -0,0 +1,38 @@
+import logging
+import json
+import importlib_resources
+
+aiecommon_resources = importlib_resources.files("aiecommon")
+
+class AieError():
+    
+    ERROR_EXCEPTION_AINERGY: str = "errorAinergy"
+    ERROR_EXCEPTION_PYTHON: str = "errorPython"
+    ERROR_INPUT_VALIDATION: str = "inputValidation"
+    
+    translations = json.load((aiecommon_resources / "data" / "translations.json").open())
+
+    def __init__(self, error, code, message, source = "NO SET"):
+
+        self.error  = error
+        self.message = message
+        self.source = source
+        self.code = code
+
+    @staticmethod
+    def translateErrorMessage(message):
+        if message in AieError.translations:
+            return AieError.translations[message]
+        else:
+            return message
+
+    @staticmethod
+    def error(error, code, source, exception = None):
+        errorDict = AieError(error, code, AieError.translateErrorMessage(code), source).__dict__
+
+        logging.info(f"AieError occured: {errorDict}, Exception Name: {type(exception).__name__}, exception={exception}")
+
+        if (exception is not None):
+            logging.exception("Caught exception:")
+
+        return errorDict
