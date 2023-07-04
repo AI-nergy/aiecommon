c82dd4c0951a0e375e1e1473123a24537d59c829
Pepe Una <xmak@studioartlan.com>
2023-07-03 21:38:41 -0500

    update Location model

--- src/aiecommon/Models/Location.py
+++ src/aiecommon/Models/Location.py
@@ -1,11 +1,9 @@
-from typing import List, Optional, Union
-
 from pydantic import BaseModel
+from typing import Optional
 
 
 class Location(BaseModel):
-    latitude: float = 55.6996798173897
-    longitude: float = 12.552480697631838
-    zip: Optional[str] = "8930"
-    countryCode: Optional[Union[str, None]] = None
+    latitude: float
+    longitude: float
+    countryCode: Optional[str]
     biddingZone: Optional[str]
