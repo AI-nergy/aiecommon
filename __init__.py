f313de3be70e3233a86baf1cd54a14aae127fc73
AmosSchledorn <amos.schledorn@icloud.com>
2023-07-09 19:42:32 +0200

    added module __init__

---
+++ __init__.py
@@ -0,0 +1,6 @@
+
+from .src.aiecommon.DataModels import *
+from .src.aiecommon.FileSystem import *
+from .src.aiecommon.Models import *
+from src.aiecommon.Output import *
+from src.aiecommon.SolarUtils import *
