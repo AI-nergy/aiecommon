f6178df91e6e26b8ce43d71452fa902b6f0e731f
AmosSchledorn <amos.schledorn@icloud.com>
2023-07-09 16:37:21 +0200

    moved Entities to Models

--- src/aiecommon/Entities/Rooftop.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel
-# from ..Output.Orientation import Orientation
-
-class Rooftop(BaseModel):
-    usefulAream2: int
-    orientation: str
-    orientationDegrees: int
-    rooftopSideSlopeDegrees: int
--- src/aiecommon/Models/__init__.py
+++ src/aiecommon/Models/__init__.py
@@ -2,3 +2,12 @@ from .Country import Country
 from .Location import Location
 from .InputData import InputData
 from .Response import ResponseBody, Rooftop
+from .Battery import Battery
+from .BiddingRegion import BiddingRegion
+from .ElectricVehicle import ElectricVehicle
+from .Inverter import Inverter
+from .Photovoltaic import Photovoltaic
+from .PricesTechnologies import PricesTechnologies
+from .Scenario import Scenario
+from .SurfaceCapacityRatio import SurfaceCapacityRatio
+from .Technologies import Technologies
