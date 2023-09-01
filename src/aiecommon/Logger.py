4f996eb4da9b0a6b080e10a66523cbfbf4fc2276
Ignacio Blanco <ibi.46.nb@gmail.com>
2023-09-01 17:55:11 +0200

    Fix logger

--- src/aiecommon/Logger.py
+++ src/aiecommon/Logger.py
@@ -3,57 +3,35 @@ import logging
 import os
 
 class Logger:
-    """
-    A class for generating and configuring loggers
-
-    ...
-
-    Methods
-    -------
-    generate_logger()
-        Generates a logger object
-    """
-    def __init__(self, path) -> None:
+    def __init__(self, path: str) -> None:
+        """
+        Initializes the Logger object.
+        """
         self.path = path
-        # Create folder in case it does not exist
-        if not os.path.exists(self.path):
-            os.makedirs(self.path)
-
-
-
-    def generate_logger(self):
-        """
-        Generates a logger object
-
-        Returns
-        -------
-        logger object
-            An instance of the configured logger
-        """
+        self.log_filename = self._generate_log_filename()
+        self._create_log_folder()
         self._setup_logger()
 
-        return logging.getLogger(__name__)
+    def _generate_log_filename(self) -> str:
+        """
+        Generates the log file name with a timestamp.
+        """
+        timestamp = datetime.datetime.now().strftime("%Y_%m_%d_%H%M%S")
+        return f"{self.path}/log_info_{timestamp}.log"
 
-    def _setup_logger(self):
+    def _create_log_folder(self) -> None:
         """
-        Configures the logging settings for the logger
+        Creates the log folder if it doesn't exist.
         """
-        # configure logging
+        os.makedirs(self.path, exist_ok=True)
+
+    def _setup_logger(self) -> None:
+        """
+        Configures the logging settings for the logger.
+        """
         logging.basicConfig(
             level=logging.INFO,
             format="%(asctime)s %(filename)-5s %(levelname)-8s %(message)s",
             datefmt="%d/%m/%Y %H:%M:%S",
-            filemode="a",
-            filename=self._get_log_filename()
+            filename=self.log_filename
         )
-
-    def _get_log_filename(self):
-        """
-        Creates a log file name based on the current time
-        Returns
-        -------
-        str
-            log filename in the format 'log_info_{ref_id}_{current_time}.log'
-        """
-        current_time = datetime.datetime.now().strftime("%Y_%m_%d_%H%M%S")
-        return f"{self.path}/log_info_{current_time}.log"
