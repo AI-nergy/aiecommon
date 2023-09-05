6684ba27bfbdfce1ee77c236abf14728dea505aa
Pepe Una <xmak@studioartlan.com>
2023-09-05 15:54:46 -0500

    celanup merge errors in CountryData

--- src/aiecommon/DataModels/CountryData.py
+++ src/aiecommon/DataModels/CountryData.py
@@ -3,9 +3,9 @@ from typing import Optional
 import json
 import logging
 
-from ..Models import BiddingRegion, PricesTechnologies
+from .BiddingRegion import BiddingRegion
+from .PricesTechnologies import PricesTechnologies
 from ..Exceptions import AieException
-from .RequestData import RequestData
 from .data_model_base import DataModelBase
 
 class CountryData(DataModelBase):
@@ -64,18 +64,3 @@ class CountryData(DataModelBase):
             logging.error(f"Country code {key} not supported for optimisation")
             raise AieException(AieException.COUNTRY_NOT_SUPPORTED_FOR_OPTIMIZATION)
 
-
-    def from_json_old(path: str, request: RequestData = None):
-        # Open the specified JSON file
-        with open(path, 'r') as f:
-            # Load the contents of the file into a Python object using json library
-            country_data = json.loads(f.read())
-
-        # Check if input countryCode is present in the loaded data or not.
-        if request.location.countryCode.upper() not in country_data:
-            # If not raise an AieException.COUNTRY_NOT_SUPPORTED_FOR_OPTIMIZATION exception
-            logging.error(f"Country code {request.location.countryCode} not supported")
-            raise AieException(AieException.COUNTRY_NOT_SUPPORTED_FOR_OPTIMIZATION)
-        
-        # Return an instance of CountryData class representing data for the input country code
-        return CountryData(**country_data[request.location.countryCode])
