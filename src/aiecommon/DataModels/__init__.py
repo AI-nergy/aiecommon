53861bb0018a91e829b203a9144cfc2affe33989
AmosSchledorn <amos.schledorn@icloud.com>
2023-09-17 14:44:50 +0200

    update _inits_

--- src/aiecommon/DataModels/__init__.py
+++ src/aiecommon/DataModels/__init__.py
@@ -2,3 +2,4 @@ from .BiddingRegion import BiddingRegion
 from .PricesTechnologies import PricesTechnologies
 from .Technologies import Technologies
 from .SystemOptimisationType import SystemOptimisationType
+from .data_model_base import DataModelBase
