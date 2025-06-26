81d11b09a84c76751a83320a1581ec1fdca3d5b2
Pepe Una <pepe@ai-nergy.net>
2025-06-26 13:28:31 -0500

    SOLP-2094: Implement MainLocation

--- src/aiecommon/Models/Location.py
+++ src/aiecommon/Models/Location.py
@@ -4,6 +4,9 @@ from typing import Optional
 class Location(BaseModel):
     latitude: float
     longitude: float
+
+
+class MainLocation(Location):
     countryCode: Optional[str] = None
     biddingZone: Optional[str] = None
     region: Optional[str] = None
