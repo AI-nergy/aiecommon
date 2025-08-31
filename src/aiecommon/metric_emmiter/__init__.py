256531daed24817460b650ae25e2045d999eb26a
Pepe Una <pepe@ai-nergy.net>
2025-08-30 20:59:12 -0500

    add metric_name to log

--- src/aiecommon/metric_emmiter/__init__.py
+++ src/aiecommon/metric_emmiter/__init__.py
@@ -59,7 +59,7 @@ class AwsMetricEmitterBase:
                     last_flush = time.time()
 
     def emit_metric(self, value: int):
-        logger.info(f"AwsMetricEmitterBase: emit_metric, value={value}")
+        logger.info(f"AwsMetricEmitterBase: emit_metric, metric_name={self.metric_name}, value={value}")
         asyncio.create_task(asyncio.to_thread(self._emit_metric, value))
 
     def _emit_metric(self, value: int):
