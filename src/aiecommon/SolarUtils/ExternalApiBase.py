dbc06b97d0fbd78b86886e2f861a98c8b8d236ef
Pepe Una <pepe@ai-nergy.net>
2025-10-26 08:28:49 -0500

    move cache and retry logic from PvGis to ExternalApiBase

--- src/aiecommon/SolarUtils/ExternalApiBase.py
+++ src/aiecommon/SolarUtils/ExternalApiBase.py
@@ -1,15 +1,13 @@
 import os
 import random
 import pandas as pd
+from  typing import Callable
+import time
+
 import aiecommon.custom_logger as custom_logger
 logger = custom_logger.get_logger()
 
-import time
-from pvlib import iotools
-import numpy as np
-
 from aiecommon.Exceptions import AieException
-from aiecommon import SolarUtils
 from aiecommon.FileSystem import LocalRuntimeFiles
 
 class ExternalApiBase():
@@ -18,28 +16,102 @@ class ExternalApiBase():
     API_IDENTIFIER = 'NONE'
     USE_PERMANENT_STORAGE = False
 
+    def __init__(self,
+        max_retries: int = 3,
+        min_retry_delay: int = 2,
+        min_result_size: int = 1024,
+        ignore_cache: bool = False,
+    ):
+        """
+        max_retries=3
+        min_retry_delay=2
+        min_result_size (optional, with default) - if the downloaded files is smaller, it won’t count as successful download
+        ignore_cache (optional, default False) - whether to download regardless of the existence of cache
+        """
+        pass
+
+        self.max_retries = max_retries
+        self.min_retry_delay = min_retry_delay
+        self.min_result_size = min_result_size
+        self.ignore_cache = ignore_cache
+        
+        os.makedirs(LocalRuntimeFiles.get_file(self.STORAGE_FOLDER, usePermanentStorage=self.USE_PERMANENT_STORAGE), exist_ok=True)
+
     @classmethod
-    def _get_cache_file_path(cls, latitude, longitude):
-        cache_key = cls._get_cache_key(latitude, longitude)
+    def _get_cache_file_path(cls, params: dict):
+        cache_key = cls._get_cache_key(params)
         return LocalRuntimeFiles.get_file(os.path.join(cls.STORAGE_FOLDER, cache_key), usePermanentStorage=cls.USE_PERMANENT_STORAGE)
 
     @classmethod
-    def _save_cache(cls, latitude, longitude, data):
-        cache_file_path = cls._get_cache_file_path(latitude, longitude)
-        logger.info(f"ExternalApiBase: Saving {cls.API_IDENTIFIER} data to cache, cache_file_path={cache_file_path}")
+    def _save_cache(cls, params: dict, data):
+        cache_file_path = cls._get_cache_file_path(params)
+        logger.info(f"ExternalApiBase/{cls.API_IDENTIFIER}: Saving {cls.API_IDENTIFIER} data to cache, cache_file_path={cache_file_path}")
         return cls._write_cache(cache_file_path, data)
 
     @classmethod
-    def _get_cache(cls, latitude, longitude):
-        full_cache_file_path = cls._get_cache_file_path(latitude, longitude)
+    def _get_cache(cls, params: dict):
+        full_cache_file_path = cls._get_cache_file_path(params)
 
         if not os.path.exists(full_cache_file_path):
-            logger.info(f"ExternalApiBase: Get {cls.API_IDENTIFIER} cache file doesn't exist, full_cache_file_path={full_cache_file_path}")
+            logger.info(f"ExternalApiBase/{cls.API_IDENTIFIER}: Get {cls.API_IDENTIFIER} cache file doesn't exist, full_cache_file_path={full_cache_file_path}")
             return None
         
         try:
-            logger.info(f"ExternalApiBase: Get {cls.API_IDENTIFIER} cache file, full_cache_file_path={full_cache_file_path}")
+            logger.info(f"ExternalApiBase/{cls.API_IDENTIFIER}: Get {cls.API_IDENTIFIER} cache file, full_cache_file_path={full_cache_file_path}")
             return cls._read_cache(full_cache_file_path)
         except Exception as e:
-            logger.error(f"ExternalApiBase: Cannot open {cls.API_IDENTIFIER} cache file, full_cache_file_path={full_cache_file_path}, exception={e}")
+            logger.error(f"ExternalApiBase/{cls.API_IDENTIFIER}: Cannot open {cls.API_IDENTIFIER} cache file, full_cache_file_path={full_cache_file_path}, exception={e}")
             return None
+
+    def call_api(
+        self,
+        api_call_function: Callable,
+        api_call_params: dict,
+        get_result_size_function: Callable,
+        max_retries: int | None = None,
+        min_retry_delay: int | None = None,
+        min_result_size: int | None = None,
+        ignore_cache: bool | None = None,
+    ) -> pd.DataFrame | None:
+        """
+        Call external api with caching and retry
+        """
+
+        max_retries = max_retries if max_retries is not None else self.max_retries
+        min_retry_delay = min_retry_delay if min_retry_delay is not None else self.min_retry_delay
+        min_result_size = min_result_size if min_result_size is not None else self.min_result_size
+        ignore_cache = ignore_cache if ignore_cache is not None else self.ignore_cache
+
+        retry_count = 0
+
+        logger.info(f"ExternalApiBase/{self.API_IDENTIFIER}: Try to get result from cache, api_call_params={api_call_params}")
+        cached_result = self._get_cache(api_call_params)
+        if cached_result is not None:
+            logger.info(f"ExternalApiBase/{self.API_IDENTIFIER}: Got result from cache, api_call_params={api_call_params}")
+            if self._check_cache(cached_result, api_call_params):
+                return cached_result
+            else:
+                logger.warning(f"ExternalApiBase/{self.API_IDENTIFIER}: cached data exsist but it didn't pass cache check, api_call_params={api_call_params}")
+        else:
+            logger.info(f"ExternalApiBase/{self.API_IDENTIFIER}: no cached data, proceeding with api request, api_call_params={api_call_params}")
+
+        while retry_count <= max_retries:
+            try:
+                result_data = api_call_function(max_retries, retry_count, **api_call_params)
+                result_size = get_result_size_function(result_data)
+
+                if  result_size < min_result_size:
+                    raise Exception(f"The result size is smaller than limit, result_size={result_size}, min_result_size={min_result_size}")
+
+                logger.info(f"ExternalApiBase/{self.API_IDENTIFIER} {retry_count}/{max_retries}: Saving result to cache, api_call_params={api_call_params}")
+                self._save_cache(api_call_params, result_data)
+
+                return result_data
+
+            except Exception as e:
+                logger.warning(f"ExternalApiBase/{self.API_IDENTIFIER} {retry_count}/{max_retries}: Caught excepption while getting data")
+                logger.warning(e)
+                retry_count += 1
+                time.sleep(self.min_retry_delay * 2**(retry_count - 1) + random.random())
+
+        raise AieException(AieException.EXTERNAL_API_FAILED, f"ExternalApiBase/{self.API_IDENTIFIER} failed after {retry_count} retries", {"api": self.API_IDENTIFIER})
--- src/aiecommon/SolarUtils/PvGis.py
+++ src/aiecommon/SolarUtils/PvGis.py
@@ -1,16 +1,11 @@
-import os
-import random
 import pandas as pd
-import aiecommon.custom_logger as custom_logger
-logger = custom_logger.get_logger()
-
 import time
 from pvlib import iotools
 import numpy as np
 
-from aiecommon.Exceptions import AieException
+import aiecommon.custom_logger as custom_logger
+logger = custom_logger.get_logger()
 from aiecommon import SolarUtils
-from aiecommon.FileSystem import LocalRuntimeFiles
 from aiecommon.SolarUtils.ExternalApiBase import ExternalApiBase
 
 class PvGis(ExternalApiBase):
@@ -42,19 +37,19 @@ class PvGis(ExternalApiBase):
         pass
 
         self.country_code = country_code
-        self.max_retries = max_retries
-        self.min_retry_delay = min_retry_delay
-        self.min_file_size = min_file_size
-        self.ignore_cache = ignore_cache
+        # self.max_retries = max_retries
+        # self.min_retry_delay = min_retry_delay
+        # self.min_file_size = min_file_size
+        # self.ignore_cache = ignore_cache
         
-        os.makedirs(LocalRuntimeFiles.get_file(PvGis.STORAGE_FOLDER, usePermanentStorage=True), exist_ok=True)
+        # os.makedirs(LocalRuntimeFiles.get_file(PvGis.STORAGE_FOLDER, usePermanentStorage=True), exist_ok=True)
 
-        super().__init__()
+        super().__init__(max_retries, min_retry_delay, min_file_size, ignore_cache)
 
 
     @staticmethod
-    def _get_cache_key(latitude: float, longitude: float):
-        return f"{PvGis.PVGIS_START_YEAR}_{PvGis.PVGIS_END_YEAR}_{np.round(latitude, PvGis.COORDINATES_DECIMAL_PLACES):.3f}_{np.round(longitude, PvGis.COORDINATES_DECIMAL_PLACES):.3f}"
+    def _get_cache_key(params: dict):
+        return f"{PvGis.PVGIS_START_YEAR}_{PvGis.PVGIS_END_YEAR}_{np.round(params['latitude'], PvGis.COORDINATES_DECIMAL_PLACES):.3f}_{np.round(params['longitude'], PvGis.COORDINATES_DECIMAL_PLACES):.3f}"
 
     @staticmethod
     def _read_cache(cache_file_path: str) -> pd.DataFrame:
@@ -64,33 +59,51 @@ class PvGis(ExternalApiBase):
     def _write_cache(cache_file_path: str, data: pd.DataFrame):
         return data.to_pickle(cache_file_path)
 
+    @staticmethod
+    def _check_cache(cached_result, params):
+        if isinstance(cached_result, pd.DataFrame) and not cached_result.empty:
+            return True
+        else:
+            logger.warning(f"PvGis: cached data exsist but it's not a pd.DataFrame or is empty, params={params}")
+            False
 
     # @staticmethod
-    # def _get_cache_file_path(latitude, longitude):
-    #     cache_key = PvGis._get_cache_key(latitude, longitude)
-    #     return LocalRuntimeFiles.get_file(os.path.join(PvGis.STORAGE_FOLDER, cache_key), usePermanentStorage=True)
+    # def _get_result_size(result_data):
+    #     logger.warning(f"PvGis: _check_result_size, result_data.size={result_data.size}")
+    #     return result_data.size
 
-    # @staticmethod
-    # def _save_cache(latitude, longitude, data: pd.DataFrame):
-    #     cache_file_path = PvGis._get_cache_file_path(latitude, longitude)
-    #     logger.info(f"PvGis: Saving data cache as pickle, cache_file_path={cache_file_path}")
-    #     data.to_pickle(cache_file_path)
+    def _fetch(self, max_retries, retry_count, latitude, longitude, country_code):
+        latitude_truncated = np.round(latitude, PvGis.COORDINATES_DECIMAL_PLACES)
+        longitude_truncated = np.round(longitude, PvGis.COORDINATES_DECIMAL_PLACES)
+        tz = SolarUtils.get_timezone_from_country_code(country_code)
 
-    # @staticmethod
-    # def _get_cache(latitude, longitude) -> pd.DataFrame:
-    #     cache_file_path = PvGis._get_cache_file_path(latitude, longitude)
+        start = pd.Timestamp(PvGis.TYPICAL_YEAR_START_DATE)
+        end = pd.Timestamp(PvGis.TYPICAL_YEAR_END_DATE)
+        logger.info(f"PvGis {retry_count}/{max_retries}: Fetching PVGIS TMY for ({latitude},{longitude}) -> ({latitude_truncated}, {longitude_truncated})")
+        start_api = time.perf_counter()
+        tmy_data = iotools.get_pvgis_tmy(
+            latitude_truncated,
+            longitude_truncated,
+            url='https://re.jrc.ec.europa.eu/api/v5_3/',
+            usehorizon=True,
+            startyear=PvGis.PVGIS_START_YEAR,
+            endyear=PvGis.PVGIS_END_YEAR,
+            coerce_year=PvGis.TYPICAL_YEAR
+        )
+        elapsed_api = time.perf_counter() - start_api
+        logger.info(f"PvGis {retry_count}/{max_retries}: PVGIS TMY fetch took {elapsed_api:.2f} seconds")
 
-    #     if not os.path.exists(cache_file_path):
-    #         logger.info(f"Get PvGis cache file doesn;t exist, cache_file_path={cache_file_path}")
-    #         return None
-        
-    #     try:
-    #         logger.info(f"Get PvGis cache file, cache_file_path={cache_file_path}")
-    #         return PvGis._read_cache(cache_file_path)
-    #     except Exception as e:
-    #         logger.error(f"Cannot open PvGis cache file, full_cache_file_path={cache_file_path}, exception={e}")
-    #         return None
+        df = tmy_data[0]
+        delays = PvGis.get_tmy_minute_offsets(latitude, longitude, tmy_data[1])
+        df.index = df.index.tz_convert(tz)
+        full = pd.date_range(start=start, end=end, freq='h', tz=tz)
+        df = df.reindex(full)
+        df.index = df.index + pd.to_timedelta(delays, unit='min')
+        df = df.bfill().ffill().interpolate(method='nearest')
+        logger.info(f"PvGis {retry_count}/{max_retries}: TMY data received; proceeding with geometry cache and production computation.")
+        final_data = df[~df.index.duplicated()]
 
+        return final_data
 
     def get_solar_components(
         self,
@@ -100,8 +113,6 @@ class PvGis(ExternalApiBase):
         min_retry_delay : int | None = None,
         min_file_size : int | None = None,
         ignore_cache : bool | None = None,
-        # start_date, end_date, tz2,
-        # max_retries=3, initial_delay=1
     ) -> pd.DataFrame | None:
         """
         Retrieve PVGIS TMY data for the given coordinates and date range,
@@ -112,68 +123,20 @@ class PvGis(ExternalApiBase):
         """
 
         country_code = country_code if country_code is not None else self.country_code
-        max_retries = max_retries if max_retries is not None else self.max_retries
-        min_retry_delay = min_retry_delay if min_retry_delay is not None else self.min_retry_delay
-        min_file_size = min_file_size if min_file_size is not None else self.min_file_size
-        ignore_cache = ignore_cache if ignore_cache is not None else self.ignore_cache
-        tz = SolarUtils.get_timezone_from_country_code(country_code)
 
-        retry_count = 0
-
-        logger.info(f"PvGis: Try to get PvGis result from cache as pickle, latitude={latitude}, longitude={longitude}")
-        cached_result = PvGis._get_cache(latitude, longitude)
-        if cached_result is not None:
-            logger.info(f"PvGis: Got PvGis result from cache as pickle, latitude={latitude}, longitude={longitude}")
-            if isinstance(cached_result, pd.DataFrame) and not cached_result.empty:
-                return cached_result
-            else:
-                logger.warning(f"PvGis: cached data exsist but it's not a pd.DataFrame or is empty, latitude={latitude}, longitude={longitude}")
-        else:
-            logger.info(f"PvGis: no cached data, proceeding with pvgis request, latitude={latitude}, longitude={longitude}")
-
-        latitude_truncated = np.round(latitude, PvGis.COORDINATES_DECIMAL_PLACES)
-        longitude_truncated = np.round(longitude, PvGis.COORDINATES_DECIMAL_PLACES),
-
-        while retry_count <= self.max_retries:
-            try:
-                start = pd.Timestamp(PvGis.TYPICAL_YEAR_START_DATE)
-                end = pd.Timestamp(PvGis.TYPICAL_YEAR_END_DATE)
-                logger.info(f"PvGis {retry_count}/{self.max_retries}: Fetching PVGIS TMY for ({latitude},{longitude}) -> ({latitude_truncated}, {longitude_truncated})")
-                start_api = time.perf_counter()
-                tmy_data = iotools.get_pvgis_tmy(
-                    latitude_truncated,
-                    longitude_truncated,
-                    url='https://re.jrc.ec.europa.eu/api/v5_3/',
-                    usehorizon=True,
-                    startyear=PvGis.PVGIS_START_YEAR,
-                    endyear=PvGis.PVGIS_END_YEAR,
-                    coerce_year=PvGis.TYPICAL_YEAR
-                )
-                elapsed_api = time.perf_counter() - start_api
-                logger.info(f"PvGis {retry_count}/{self.max_retries}: PVGIS TMY fetch took {elapsed_api:.2f} seconds")
-
-                df = tmy_data[0]
-                delays = PvGis.get_tmy_minute_offsets(latitude, longitude, tmy_data[1])
-                df.index = df.index.tz_convert(tz)
-                full = pd.date_range(start=start, end=end, freq='h', tz=tz)
-                df = df.reindex(full)
-                df.index = df.index + pd.to_timedelta(delays, unit='min')
-                df = df.bfill().ffill().interpolate(method='nearest')
-                logger.info(f"PvGis {retry_count}/{self.max_retries}: TMY data received; proceeding with geometry cache and production computation.")
-                final_data = df[~df.index.duplicated()]
-
-                logger.info(f"PvGis {retry_count}/{self.max_retries}: Saving result to cache as pickle, latitude={latitude}, longitude={longitude}")
-                PvGis._save_cache(latitude, longitude, final_data)
-
-                return final_data
-            
-            except Exception as e:
-                logger.warning(f"PvGis {retry_count}/{self.max_retries}: Caught excepption while getting pvgis data")
-                logger.warning(e)
-                retry_count += 1
-                time.sleep(self.min_retry_delay * 2**(retry_count - 1) + random.random())
-
-        raise AieException(AieException.EXTERNAL_API_FAILED, f"PvGis failed after {retry_count} retries", {"api": "pvgis"})
+        return self.call_api(
+            api_call_function=self._fetch,
+            api_call_params={
+                "latitude": latitude,
+                "longitude": longitude,
+                "country_code": country_code,
+            },
+            get_result_size_function=lambda result_data: result_data.size,
+            max_retries=max_retries,
+            min_retry_delay=min_retry_delay,
+            min_result_size=min_file_size,
+            ignore_cache=ignore_cache,
+        )
 
 
     @staticmethod
